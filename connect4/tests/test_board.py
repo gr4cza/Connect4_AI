49250d2788f394171c0e42afb23734d1b6977f1b
gr4cza <graczadavid@gmail.com>
2019-11-09 13:51:55 +0100

    Some changes and add draw to tests

--- connect4/tests/test_board.py
+++ connect4/tests/test_board.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from board import Board
+from board import Board, NO_ONE, PLAYER1, PLAYER2
 
 
 class MyTestCase(unittest.TestCase):
@@ -38,33 +38,33 @@ class MyTestCase(unittest.TestCase):
                                            '[O, O, X,  ,  ,  , O]\n'))
 
     def test_invalid_moves(self):
-        self.board.board = np.array([[0, 0, 0, 2, 0, 0, 0],
-                                     [0, 0, 0, 2, 0, 0, 0],
-                                     [0, 0, 0, 2, 0, 0, 0],
-                                     [0, 0, 0, 2, 0, 0, 0],
-                                     [0, 0, 0, 2, 0, 0, 0],
-                                     [0, 0, 0, 2, 0, 0, 0]])
+        self.board.board = np.array([[NO_ONE, NO_ONE, NO_ONE, PLAYER2, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, NO_ONE, NO_ONE, PLAYER2, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, NO_ONE, NO_ONE, PLAYER2, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, NO_ONE, NO_ONE, PLAYER2, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, NO_ONE, NO_ONE, PLAYER2, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, NO_ONE, NO_ONE, PLAYER2, NO_ONE, NO_ONE, NO_ONE]])
         self.assertFalse(self.board.add_token(3))
 
     def test_empty_board_available_moves(self):
         self.assertEqual(self.board.available_moves(), [0, 1, 2, 3, 4, 5, 6])
 
     def test_one_full_column_available_moves(self):
-        self.board.board = np.array([[0, 1, 0, 0, 0, 0, 0],
-                                     [0, 1, 0, 0, 0, 0, 0],
-                                     [0, 1, 0, 0, 0, 0, 0],
-                                     [0, 1, 0, 0, 0, 0, 0],
-                                     [0, 1, 0, 0, 0, 0, 0],
-                                     [0, 1, 0, 0, 0, 0, 0]])
+        self.board.board = np.array([[NO_ONE, PLAYER1, NO_ONE, NO_ONE, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, PLAYER1, NO_ONE, NO_ONE, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, PLAYER1, NO_ONE, NO_ONE, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, PLAYER1, NO_ONE, NO_ONE, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, PLAYER1, NO_ONE, NO_ONE, NO_ONE, NO_ONE, NO_ONE],
+                                     [NO_ONE, PLAYER1, NO_ONE, NO_ONE, NO_ONE, NO_ONE, NO_ONE]])
         self.assertEqual(self.board.available_moves(), [0, 2, 3, 4, 5, 6])
 
     def test_full_table_available_moves(self):
-        self.board.board = np.array([[2, 1, 2, 1, 2, 1, 1],
-                                     [2, 1, 2, 1, 2, 1, 1],
-                                     [2, 1, 2, 1, 2, 1, 1],
-                                     [2, 1, 2, 1, 2, 1, 1],
-                                     [2, 1, 2, 1, 2, 1, 1],
-                                     [2, 1, 2, 1, 2, 1, 1]])
+        self.board.board = np.array([[PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER1],
+                                     [PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER1],
+                                     [PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER1],
+                                     [PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER1],
+                                     [PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER1],
+                                     [PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER2, PLAYER1, PLAYER1]])
         self.assertEqual(self.board.available_moves(), [])
 
     def test_no_winner_board(self):
@@ -76,7 +76,7 @@ class MyTestCase(unittest.TestCase):
                                            '[ ,  , O,  ,  ,  ,  ]\n'
                                            '[ ,  , O, X,  ,  , X]\n'
                                            '[O, X, O, X, X,  , O]\n'))
-        self.assertEqual(self.board.check_win(), 0)
+        self.assertEqual(self.board.get_winner(), NO_ONE)
 
     def test_player_one_win(self):
         for i in [1, 6, 2, 6, 3, 5, 4]:
@@ -87,7 +87,7 @@ class MyTestCase(unittest.TestCase):
                                            '[ ,  ,  ,  ,  ,  ,  ]\n'
                                            '[ ,  ,  ,  ,  ,  , X]\n'
                                            '[ , O, O, O, O, X, X]\n'))
-        self.assertEqual(self.board.check_win(), 1)
+        self.assertEqual(self.board.get_winner(), PLAYER1)
 
     def test_player_two_win(self):
         for i in [0, 1, 6, 2, 6, 3, 5, 4]:
@@ -98,7 +98,7 @@ class MyTestCase(unittest.TestCase):
                                            '[ ,  ,  ,  ,  ,  ,  ]\n'
                                            '[ ,  ,  ,  ,  ,  , O]\n'
                                            '[O, X, X, X, X, O, O]\n'))
-        self.assertEqual(self.board.check_win(), 2)
+        self.assertEqual(self.board.get_winner(), PLAYER2)
 
     def test_row_win(self):
         for i in [0, 1, 2, 1, 2, 3, 4, 5, 6, 1, 3, 6, 5, 0, 4]:
@@ -109,7 +109,7 @@ class MyTestCase(unittest.TestCase):
                                            '[ , X,  ,  ,  ,  ,  ]\n'
                                            '[X, X, O, O, O, O, X]\n'
                                            '[O, X, O, X, O, X, O]\n'))
-        self.assertEqual(self.board.check_win(), 1)
+        self.assertEqual(self.board.get_winner(), PLAYER1)
 
     def test_column_win(self):
         for i in [0, 1, 2, 1, 2, 3, 2, 4, 2]:
@@ -120,7 +120,7 @@ class MyTestCase(unittest.TestCase):
                                            '[ ,  , O,  ,  ,  ,  ]\n'
                                            '[ , X, O,  ,  ,  ,  ]\n'
                                            '[O, X, O, X, X,  ,  ]\n'))
-        self.assertEqual(self.board.check_win(), 1)
+        self.assertEqual(self.board.get_winner(), PLAYER1)
 
     def test_column_not_bottom_win(self):
         for i in [0, 1, 1, 2, 1, 2, 1, 2, 1]:
@@ -131,7 +131,7 @@ class MyTestCase(unittest.TestCase):
                                            '[ , O, X,  ,  ,  ,  ]\n'
                                            '[ , O, X,  ,  ,  ,  ]\n'
                                            '[O, X, X,  ,  ,  ,  ]\n'))
-        self.assertEqual(self.board.check_win(), 1)
+        self.assertEqual(self.board.get_winner(), PLAYER1)
 
     def test_positive_diagonal(self):
         for i in [0, 1, 1, 2, 2, 3, 2, 3, 3, 5, 3]:
@@ -142,7 +142,7 @@ class MyTestCase(unittest.TestCase):
                                            '[ ,  , O, O,  ,  ,  ]\n'
                                            '[ , O, O, X,  ,  ,  ]\n'
                                            '[O, X, X, X,  , X,  ]\n'))
-        self.assertEqual(self.board.check_win(), 1)
+        self.assertEqual(self.board.get_winner(), PLAYER1)
 
     def test_negative_diagonal(self):
         for i in [0, 0, 0, 0, 1, 1, 2, 1, 4, 2, 6, 3]:
@@ -153,7 +153,7 @@ class MyTestCase(unittest.TestCase):
                                            '[O, X,  ,  ,  ,  ,  ]\n'
                                            '[X, X, X,  ,  ,  ,  ]\n'
                                            '[O, O, O, X, O,  , O]\n'))
-        self.assertEqual(self.board.check_win(), 2)
+        self.assertEqual(self.board.get_winner(), PLAYER2)
 
     def test_diagonal_center(self):
         for i in [3, 2, 4, 1, 1, 2, 2, 3, 3, 4, 3, 4, 6, 4, 4]:
@@ -164,10 +164,19 @@ class MyTestCase(unittest.TestCase):
                                            '[ ,  , O, O, X,  ,  ]\n'
                                            '[ , O, X, X, X,  ,  ]\n'
                                            '[ , X, X, O, O,  , O]\n'))
-        self.assertEqual(self.board.check_win(), 1)
+        self.assertEqual(self.board.get_winner(), PLAYER1)
 
     def test_draw(self):
-        pass
+        for i in [3, 3, 3, 4, 1, 0, 0, 3, 2, 0, 6, 1, 4, 2, 1, 4, 4, 1, 0, 5, 5, 3, 2, 0, 3, 2, 1,
+                  4, 2, 5, 4, 0, 5, 5, 1, 6, 5, 2, 6, 6, 6, 6]:
+            self.board.add_token(i)
+        self.assertEqual(str(self.board), ('[X, O, X, O, O, O, X]\n'
+                                           '[X, O, O, X, X, X, O]\n'
+                                           '[O, X, X, X, O, O, X]\n'
+                                           '[X, O, O, O, X, X, O]\n'
+                                           '[O, X, X, X, O, O, X]\n'
+                                           '[X, O, O, O, X, X, O]\n'))
+        self.assertEqual(self.board.get_winner(), NO_ONE)
 
 
 if __name__ == '__main__':
