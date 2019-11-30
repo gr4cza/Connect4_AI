13191dfbb3fe527192a692dbf5e3c8978b6909a1
gr4cza <graczadavid@gmail.com>
2019-11-30 15:29:59 +0100

    Add basic Monte Carlo Tree Search

---
+++ connect4/agent/monte_carlo.py
@@ -0,0 +1,84 @@
+from __future__ import annotations
+
+from copy import deepcopy
+from math import sqrt
+from random import choice
+
+import numpy as np
+
+from agent.random_agent import RandomAgent
+from board import PLAYER1, PLAYER2
+
+CR = 1.4142  # ~ sqrt(2)
+random_a = RandomAgent()
+
+
+class Node(object):
+    def __init__(self, board, player, parent=None):
+        self.board = board
+        self.player = player
+        self.parent = parent
+        self.children = {}
+        self.plays = 0
+        self.wins = 0
+
+        self._create_children_nodes()
+
+    def _create_children_nodes(self):
+        for move in self.board.available_moves():
+            self.children[move] = None
+
+    def UTC(self):
+        utc = self.wins / self.plays + CR * sqrt(np.log(self.parent.plays) / self.plays)
+        return utc
+
+    def expand(self) -> Node:
+        column = choice([key for (key, value) in self.children.items() if value is None])
+        board = deepcopy(self.board)
+        board.add_token(column)
+        self.children[column] = Node(board, PLAYER1 if self.player == PLAYER2 else PLAYER2, self)
+        return self.children[column]
+
+    def simulate(self):
+        board = deepcopy(self.board)
+        while not board.is_game_over():
+            col = random_a.move(board)
+            board.add_token(col)
+        return board.get_winner()
+
+    def back_propagate(self, winner):
+        self.plays += 1
+        self.wins += 1 if self.player != winner else 0
+        if self.parent:
+            self.parent.back_propagate(winner)
+
+    def __str__(self) -> str:
+        return f'{self.wins}/{self.plays} '
+
+
+class MonteCarlo(object):
+    def __init__(self, player, turns) -> None:
+        self.turns = turns
+        self.player = player
+
+    def move(self, board):
+        return self._monte_carlo(board)
+
+    def _monte_carlo(self, board):
+        root = Node(board, self.player)
+        for _ in range(self.turns):
+            node = self._select_best_node(root)
+            new_node = node.expand()
+            winner = new_node.simulate()
+            new_node.back_propagate(winner)
+        return self._best_move(root)
+
+    @staticmethod
+    def _best_move(root):
+        key, _ = max(root.children.items(), key=lambda x: x[1].plays)
+        return key
+
+    def _select_best_node(self, node) -> Node:
+        if None in node.children.values():
+            return node
+        return self._select_best_node(max(node.children.values(), key=lambda x: x.UTC()))
