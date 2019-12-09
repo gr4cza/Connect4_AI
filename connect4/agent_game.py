84a6550b47ef4f19c73d407c8a447ff113b3febb
gr4cza <graczadavid@gmail.com>
2019-12-09 02:58:26 +0100

    Small changes

--- connect4/agent_game.py
+++ connect4/agent_game.py
@@ -1,5 +1,9 @@
+from time import time
+
 from agent.min_max import MinMaxAgentWAlphaBeta
+from agent.monte_carlo import MonteCarlo
 from agent.q_learn import QLearnTrain
+from agent.random_agent import RandomAgent
 from board import Board, PLAYER1, PLAYER2
 from evaluator import BasicScore, AdvancedScore
 
@@ -17,11 +21,14 @@ def play_game(player1, player2):
             col = player2.move(board)
             board.add_token(col)
             player = PLAYER1
-    # print(board)
-    return board.get_winner()
+        print('.', end='')
+    print()
+    print(board)
+    return board.winner
 
 
 def play_n_game(n, player1, player2):
+    player1.player, player2.player = PLAYER1, PLAYER2
     p1, p2, d = 0, 0, 0
     for i in range(n):
         winner = play_game(player1, player2)
@@ -31,6 +38,7 @@ def play_n_game(n, player1, player2):
             p1 += 1
         elif winner == PLAYER2:
             p2 += 1
+        print(i)
 
     print(f'p1: {p1}, p2: {p2}, d: {d}')
 
@@ -51,7 +59,9 @@ if __name__ == '__main__':
     # play_n_game(10, RandomAgent(), MinMaxAgentWAlphaBeta(5, basic_score.score, PLAYER2))
 
     # # min-max adv first vs random
-    # play_n_game(100, MinMaxAgentWAlphaBeta(3, adv_score.score, PLAYER1), RandomAgent())
+    start_time = time()
+    play_n_game(1000,  MinMaxAgentWAlphaBeta(2, adv_score.score), RandomAgent)
+    print(time() - start_time)
     # # min-max adv second vs random
     # play_n_game(100, RandomAgent(), MinMaxAgentWAlphaBeta(3, adv_score.score, PLAYER2))
 
@@ -59,11 +69,18 @@ if __name__ == '__main__':
     # play_n_game(1, MinMaxAgentWAlphaBeta(5, adv_score.score, PLAYER1),
     #             MinMaxAgentWAlphaBeta(5, adv_score.score, PLAYER2))
 
+    # MonteCarlo vs random
+    # play_n_game(10,  RandomAgent(), MonteCarlo(PLAYER2, 5_000))
+    # play_n_game(10, MonteCarlo(10_000), MinMaxAgentWAlphaBeta(6, adv_score.score))
+    # play_n_game(10, MonteCarlo(10_000), MinMaxAgentWAlphaBeta(7, adv_score.score))
+    # play_n_game(10, MinMaxAgentWAlphaBeta(6, adv_score.score), MonteCarlo(10_000))
+    # play_n_game(10, MinMaxAgentWAlphaBeta(8, adv_score.score), MonteCarlo(10_000))
+
     # play_n_game(100_000, q_agent, RandomAgent())
     # play_n_game(100_000, RandomAgent(), RandomAgent())
     # play_n_game(1, q_agent, MinMaxAgentWAlphaBeta(5, adv_score.score, PLAYER2))
-    train = QLearnTrain(PLAYER1)
-    train.learn(100_000, against=MinMaxAgentWAlphaBeta(4, adv_score.score, PLAYER2), name=f'min_max_4_100K_p1')
-    train.player = PLAYER2
-    train.learn(100_000, against=MinMaxAgentWAlphaBeta(4, adv_score.score, PLAYER1), name=f'min_max_4_100K_both')
-    train.reset()
+    # train = QLearnTrain()
+    # train.learn(1_000, against=RandomAgent(), name=f'min_max_4_100K_p1')
+    # train.player = PLAYER2
+    # train.learn(100_000, against=MinMaxAgentWAlphaBeta(4, adv_score.score, PLAYER1), name=f'min_max_4_100K_both')
+    # train.reset()
